e53333af9a886a07accce8a2333f5aa33f714576
Pingu1337 <herman.brunberg@utb.ecutbildning.se>
2021-12-27 14:23:11 +0100

    added copy to clipboard animation

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,10 +1,17 @@
-function CopyClipboard(x) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+
+async function CopyClipboard(x) {
     /* Get the text field */
     if(x==1){
         var copyText = document.getElementById("pingu-clip");
+        var copyButton = document.querySelector(".copy-btn1");
     }
     if(x==2){
         var copyText = document.getElementById("pingu-clip-1");
+        var copyButton = document.querySelector(".copy-btn2");
     }
     
   
@@ -16,5 +23,13 @@ function CopyClipboard(x) {
     navigator.clipboard.writeText(copyText.value);
     
     /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    // alert("Copied the text: " + copyText.value);
+    copyButton.classList.add("copied");
+    copyButton.innerHTML = "<i class=\"fas fa-clipboard-check\" style=\"margin-right:25px;\"></i>Copied!"
+    await sleep(3000);
+    copyButton.classList.add("copiedback-pink");
+    copyButton.classList.remove("copied");
+    copyButton.innerHTML = "Copy To Clipboard"
+    await sleep(1000);
+    copyButton.classList.remove("copiedback-pink")
   }
