9b911fb77a165c3535e172105805efc46db13e4c
Herman Brunberg <herman.brunberg@utb.ecutbildning.se>
2022-01-14 20:47:27 +0100

    added jsHTML for components page

--- pages/assets/js/pinguScripts.js
+++ pages/assets/js/pinguScripts.js
@@ -40,8 +40,8 @@ document.addEventListener('DOMContentLoaded', async function() {
 // | called. This is done with async because the collapsed nav bar have to be loaded in order | //
 // | for the full height to be registered - default wait time is set to 100ms but if this     | //
 // | needs to be a higher value depending on client/browser needs further testing.            | //
-let resized = false;
-let media600 = window.matchMedia("(max-width: 600px)");
+var resized = false;
+var media600 = window.matchMedia("(max-width: 600px)");
 window.addEventListener('resize', function(){
     if (media600.matches && !resized){
         resized = true;
@@ -81,7 +81,6 @@ if(navToggleExists){
     document.getElementById("navtoggler").addEventListener('click', async function(){   
         var navheight = document.getElementById("pingu-nav").offsetHeight + 'px';
         document.documentElement.style.setProperty('--pl-nav-height', navheight);
-        console.log(navheight);
         document.body.classList.toggle('pushed');
         document.getElementById('pingu-nav').classList.toggle('nav-collapse');
         document.getElementById('pingu-nav').classList.toggle('nav-show');
@@ -102,22 +101,23 @@ if(navToggleExists){
 // | down within the 'pingu-swipe-area'. The function opens the nav-bar when screen width is  | //
 // | less than 600px.                                                                         | //
 var navExists = document.getElementById('pingu-nav');
-if(navExists){
     async function SwipeDownNav(){
-        // calculating the amount of pixels content will be pushed
-        var navheight = document.getElementById("pingu-nav").offsetHeight + 'px';
-        document.documentElement.style.setProperty('--pl-nav-height', navheight);
-        // adding the pushed class
-        document.body.classList.add('pushed');
-        // adding the 'nav-show' class
-        // since a down swipe only should open the menu 'classlist.add' is used here
-        document.getElementById('pingu-nav').classList.remove('nav-collapse');
-        document.getElementById('pingu-nav').classList.add('nav-show');
-        document.getElementById('pingu-nav').classList.remove('nav-hide');
-        //adding animation to nav-toggle
-        document.getElementById('navtoggle').classList.add('nav-toggle-open');
-        // hiding overflow-Y to prevent flickering when nav goes back up 
-        document.body.style.overflowY = 'hidden';
+        if(navExists){
+            // calculating the amount of pixels content will be pushed
+            var navheight = document.getElementById("pingu-nav").offsetHeight + 'px';
+            document.documentElement.style.setProperty('--pl-nav-height', navheight);
+            // adding the pushed class
+            document.body.classList.add('pushed');
+            // adding the 'nav-show' class
+            // since a down swipe only should open the menu 'classlist.add' is used here
+            document.getElementById('pingu-nav').classList.remove('nav-collapse');
+            document.getElementById('pingu-nav').classList.add('nav-show');
+            document.getElementById('pingu-nav').classList.remove('nav-hide');
+            //adding animation to nav-toggle
+            document.getElementById('navtoggle').classList.add('nav-toggle-open');
+            // hiding overflow-Y to prevent flickering when nav goes back up 
+            document.body.style.overflowY = 'hidden';
+        };
     };
 
 // |__________________________________________________________________________________________| //
@@ -125,20 +125,20 @@ if(navExists){
 // | up within the 'pingu-swipe-area'. The function closes the nav-bar when screen width is   | //
 // | less than 600px.                                                                         | //
     async function SwipeUpNav(){
-
-        // removing the 'nav-show' class
-        // since a up swipe only should close the menu 'classlist.remove' is used here
-        document.getElementById('pingu-nav').classList.remove('nav-show');
-        // removing the pushed class
-        document.body.classList.remove('pushed');
-        document.getElementById('pingu-nav').classList.add('nav-collapse');
-        //removing animation from nav-toggle
-        document.getElementById('navtoggle').classList.remove('nav-toggle-open');
-        await sleep(100);
-        // waiting for nav to start going up and then re enabling overflow-Y 
-        document.body.style.overflowY = 'auto';
+        if(navExists){
+            // removing the 'nav-show' class
+            // since a up swipe only should close the menu 'classlist.remove' is used here
+            document.getElementById('pingu-nav').classList.remove('nav-show');
+            // removing the pushed class
+            document.body.classList.remove('pushed');
+            document.getElementById('pingu-nav').classList.add('nav-collapse');
+            //removing animation from nav-toggle
+            document.getElementById('navtoggle').classList.remove('nav-toggle-open');
+            await sleep(100);
+            // waiting for nav to start going up and then re enabling overflow-Y 
+            document.body.style.overflowY = 'auto';
+        };
     };
-};
 
 // |__________________________________________________________________________________________| //
 // | This section is where the touch events are registered from the 'pingu-swipe-area' class  | //
@@ -200,28 +200,31 @@ function handleTouchMove(evt) {
 // |__________________________________________________________________________________________| //
 // |                               Slider Gradient section                                    | //
 // |__________________________________________________________________________________________| //
+function LoadSliders(){
 var sliderExists = document.querySelector('.pingu-slider');
-if(sliderExists){
-    var slider = document.querySelectorAll('.pingu-slider').forEach(s => {
-        var value = (s.value-s.min)/(s.max-s.min)*100
-        s.style.background = 'linear-gradient(to right, #e9a0a0 0%, #e9a0a0 ' + value + '%, #fce0e0 ' + value + '%, #fce0e0 100%)';
-        // Slider Label Value
-        if(s.previousElementSibling.nodeName == 'LABEL'){
-            if(s.previousElementSibling.childNodes[1].id == 'pingu-slider-value'){
-                s.previousElementSibling.childNodes[1].innerHTML = s.value;
+    if(sliderExists){
+        var slider = document.querySelectorAll('.pingu-slider').forEach(s => {
+            var value = (s.value-s.min)/(s.max-s.min)*100
+            s.style.background = 'linear-gradient(to right, #e9a0a0 0%, #e9a0a0 ' + value + '%, #fce0e0 ' + value + '%, #fce0e0 100%)';
+            // Slider Label Value
+            if(s.previousElementSibling.nodeName == 'LABEL'){
+                if(s.previousElementSibling.childNodes[1].id == 'pingu-slider-value'){
+                    s.previousElementSibling.childNodes[1].innerHTML = s.value;
+                };
             };
-        };
-        s.addEventListener('input', UpdateSlider);
-    });
-    // Function to update slider gradient dynamically
-    function UpdateSlider() {
-        var value = (this.value-this.min)/(this.max-this.min)*100
-        this.style.background = 'linear-gradient(to right, #e9a0a0 0%, #e9a0a0 ' + value + '%, #fce0e0 ' + value + '%, #fce0e0 100%)';
-        // Update Slider Label Value
-        if(this.previousElementSibling.nodeName == 'LABEL'){
-            if(this.previousElementSibling.childNodes[1].id == 'pingu-slider-value'){
-                this.previousElementSibling.childNodes[1].innerHTML = this.value;
-            };
+            s.addEventListener('input', UpdateSlider);
+        });
+    };
+};
+LoadSliders();
+// Function to update slider gradient dynamically
+function UpdateSlider() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #e9a0a0 0%, #e9a0a0 ' + value + '%, #fce0e0 ' + value + '%, #fce0e0 100%)';
+    // Update Slider Label Value
+    if(this.previousElementSibling.nodeName == 'LABEL'){
+        if(this.previousElementSibling.childNodes[1].id == 'pingu-slider-value'){
+            this.previousElementSibling.childNodes[1].innerHTML = this.value;
         };
     };
-}
+};
